89a8fd944cff7f05e973bec7d765735b48aeaf8a
drashin <drashin@codix.bg>
2020-03-02 16:05:01 +0200

    adding reducers

---
+++ src/store/reducers/authReducer.js
@@ -0,0 +1,12 @@
+const initState = {
+    projects: [
+        {id: '1', title: 'help me find peach', content: 'blac' },
+        {id: '2', title: 'help me find peach2', content: 'blac2' },
+        {id: '3', title: 'help me find peach3', content: 'blac3' }
+    ]
+}
+const authReducer = (state = initState, action) => {
+    return state;
+}
+
+export default authReducer
